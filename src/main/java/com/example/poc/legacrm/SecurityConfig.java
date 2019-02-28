c1ff575e303b55333d89101c311ba2df842e2285
Toshiaki Maki <tmaki@pivotal.io>
2019-03-01 03:14:42 +0900

    Permit accesses to WSDL

--- src/main/java/com/example/poc/legacrm/SecurityConfig.java
+++ src/main/java/com/example/poc/legacrm/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests() //
+				.antMatchers(HttpMethod.GET, "/ws/*.wsdl").permitAll() //
 				.antMatchers(HttpMethod.POST, "/ws").permitAll() //
 				.anyRequest().authenticated() //
 				.and() //
